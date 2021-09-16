903d8d8bcc1fcac5a5b66d042c2c67c9e3a333b4
Wellington <wjas.ma@gmail.com>
2021-09-16 17:23:31 -0300

    bulma adicionado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/fabricantes', action: 'fabricantes', controller: 'nome'
-  get '/carros', action: 'carros', controller: 'nome'
+  get '/fabricantes', action: 'fabricantes', controller: 'home'
+  get '/carros', action: 'carros', controller: 'home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
